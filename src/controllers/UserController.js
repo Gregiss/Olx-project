8072b343d71c3bd63f1a174d1c799ebff3f95c68
Gustavo Regis <gugaregis@hotmail.com>
2021-07-11 00:08:24 -0300

    inf usuarios

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = reqquire('../models/State')
+const User = require('../models/Users')
+const Category = require('../models/Category')
+const Ad = require('../models/Ad')
 
 module.exports = {
     getStates: async (req,res) => {
@@ -7,7 +10,40 @@ module.exports = {
         res.json({ states })
     },
     info: async (req,res) => {
+        let token = req.query.token
 
+        const user = await User.findOne({token})
+        const state = await State.findById(user.state)
+        const ads = await Ad.find({idUser: user._toString()})
+
+        let adList= []
+        for(let i in ads){
+            const cat = await Category.findById(ads[i].category)
+                        
+           // adLista.push({
+            //    id: ad[i]._id,
+             //   status:ads[i].status,
+             //   images:ads[i]images,
+             //   date.Created: ads[i]dateCrated,
+             //   title: ads.title,
+             //   price:ads.price,
+              //  priceNegotiable:ads[i].priceNegotiable,
+             //   description:ads[i].description,
+              //  views:ads[i].views,
+              //  category:ads[i].category,
+              //  category: cat.slug
+
+           // })
+                 
+            adList.push({...ads[i], category:cat.slug})
+        }
+        res.json({
+          name: user.name,
+          email: user.email,
+          state: user.name,
+          ads:adList
+
+        })
     },
     editAction: async (req,res)=> {
 
